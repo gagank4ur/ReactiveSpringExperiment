2c38ac8e796bd65847c8717e626e999a99c426ce
Gagandeep Kaur <kgagandeep@vmware.com>
2022-02-21 14:34:17 +0000

    Add parrallel example

--- src/main/kotlin/com/example/demo/BookController.kt
+++ src/main/kotlin/com/example/demo/BookController.kt
@@ -1,10 +1,10 @@
 package com.example.demo
 
-import org.reactivestreams.Publisher
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 
 @RestController
 class BookController(
@@ -19,8 +19,34 @@ class BookController(
 
     @GetMapping("/books/read")
     fun getReadBooks(): Flux<BookResponse> {
-        return bookRepository.findBooksByRead(true).flatMap(fun(book: Book): Publisher<out BookResponse> =
-            openLibraryClient.getDetails(book.isbn).map {
+        return bookRepository.findBooksByRead(true).flatMap { book ->
+            getDetails(book)
+        }
+    }
+
+    @GetMapping("/books/read/parallel")
+    fun getReadBooksParallel(): Flux<BookResponse> {
+        return bookRepository.findBooksByRead(true).parallel().runOn(Schedulers.boundedElastic()).flatMap { book ->
+            getDetails(book)
+        }.sequential()
+    }
+
+    private fun getDetails(book: Book): Mono<BookResponse> {
+        return openLibraryClient.getDetails(book.isbn).map {
+            BookResponse(
+                isbn = book.isbn,
+                title = book.title,
+                authors = book.authors.split(",").map { it.trim() },
+                read = book.read,
+                owned = book.owned,
+                dateRead = book.dateRead,
+                starRating = book.starRating,
+                publishers = it?.publishers,
+                numberOfPages = it?.numberOfPages,
+                publishDate = it?.publishDate
+            )
+        }.switchIfEmpty(
+            Mono.just(
                 BookResponse(
                     isbn = book.isbn,
                     title = book.title,
@@ -29,24 +55,9 @@ class BookController(
                     owned = book.owned,
                     dateRead = book.dateRead,
                     starRating = book.starRating,
-                    publishers = it?.publishers,
-                    numberOfPages = it?.numberOfPages,
-                    publishDate = it?.publishDate
-                )
-            }.switchIfEmpty(
-                Mono.just(
-                    BookResponse(
-                        isbn = book.isbn,
-                        title = book.title,
-                        authors = book.authors.split(",").map { it.trim() },
-                        read = book.read,
-                        owned = book.owned,
-                        dateRead = book.dateRead,
-                        starRating = book.starRating,
-                        publishers = null,
-                        numberOfPages = null,
-                        publishDate = null
-                    )
+                    publishers = null,
+                    numberOfPages = null,
+                    publishDate = null
                 )
             )
         )
